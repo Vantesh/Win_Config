bc68d9ba71d4b74e61a257b7ce4be362fc2a0e32
Victor Muthiani <121645908+Vantesh@users.noreply.github.com>
2024-06-21 00:37:44 +0300

    remove winfetch

--- auto_install.ps1
+++ auto_install.ps1
@@ -10,9 +10,7 @@ install-Module -Name Terminal-Icons -Repository PSGallery
 
 Write-Host "----------------Installing All The Apps----------------"
 winget install -e --id Microsoft.VCRedist.2015+.x64
-winget install -e --id Microsoft.VCRedist.2015+.x86
 winget install -e --id Microsoft.VCRedist.2013.x64
-winget install -e --id Microsoft.VCRedist.2013.x86
 winget install -e --id Microsoft.DotNet.Runtime.6
 winget install -e --id Microsoft.DirectX
 winget install -e --id 7zip.7zip
@@ -21,7 +19,5 @@ winget install -e --id VideoLAN.VLC
 winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id Telegram.TelegramDesktop
 winget install -e --id Starship.Starship
-scoop install winfetch
-
 Write-Host "-----------Forcing File Explorer to open to This PC instead of Quick Access--------------"
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v LaunchTo /t REG_DWORD /d 1 /f
